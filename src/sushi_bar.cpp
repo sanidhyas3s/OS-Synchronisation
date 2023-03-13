752c7b626a90a424650dda29552bac9508ea1737
Ayush Patel <ayush@Ayushs-MacBook-Air.local>
2023-03-13 12:31:16 +0530

    Updated sushi_bar.cpp

--- src/sushi_bar.cpp
+++ src/sushi_bar.cpp
@@ -2,50 +2,65 @@
 #include <semaphore.h>
 #include <pthread.h>
 #include <unistd.h>
-#define NO_OF_CUSTOMERS 18
+#define NO_OF_CUSTOMERS 25
 
 
 int eating = 0, waiting = 0; 
-sem_t *block, *mutex;
+// Semaphore block = Semaphore(0);
+// Semaphore mutex = Semaphore(1);
+// Semaphore mutex1 = Semaphore(1);
+sem_t *block, *mutex, *mutex1;
+bool must_wait = false;
 
 void* sushi_bar(void* args){
-    int n;
     int identity = *(int*)args;
     sem_wait(mutex);
-    sleep(1);
-    if(eating == 5){
+    sleep(2);
+    if(must_wait){
         waiting++;
+        printf("\tWaiting customer...%d\n", *(int*)args+1);
         sem_post(mutex);
-        printf("\tWaiting customer...%d\n", identity+1);
         sem_wait(block);
+        waiting--;
+    }
+
+    eating++;
+    printf("Eating customer...%d\n", *(int*)args+1);
+    if(eating == 5){
+        must_wait = true;
+    }
+    if(waiting && !(must_wait)){
+        sem_post(block);
     }
     else{
-        eating++;
         sem_post(mutex);
     }
-    printf("Eating customer...%d\n", identity+1);
-    sleep(3);
-    sem_wait(mutex);
+
+    sleep(12);
+
+    sem_wait(mutex1);
     eating--;
-    printf("\t\tLeaving customer...%d\n", identity+1);
-    if(eating == 0 && waiting){
-        n = fmin(waiting, 5);
-        waiting -= n;
-        eating += n;
-        for(int i = 0; i < n; i++){
-            sem_post(block);
-        }
+    printf("\t\tLeaving customer...%d\n", *(int*)args+1);
+    if(eating == 0){
+        must_wait = false;
     }
-    sem_post(mutex);
-    sleep(3);
+    if(waiting && !(must_wait)){
+        sem_post(block);
+        sem_post(mutex1);
+    }
+    else{
+        sem_post(mutex1);
+    }
     return NULL;
 }
 
 int main(){
     mutex = sem_open("mutex",O_CREAT, S_IRUSR|S_IWUSR, 1);
+    mutex1 = sem_open("mutex1",O_CREAT, S_IRUSR|S_IWUSR, 1);
     block = sem_open("block",O_CREAT, S_IRUSR|S_IWUSR, 0);
 
     sem_unlink("mutex");
+    sem_unlink("mutex1");
     sem_unlink("block");
 
     pthread_t customer[NO_OF_CUSTOMERS];
