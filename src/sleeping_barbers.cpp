ac1e041ad0d3437c15a7e22330ef5a16dac451cb
Rudransh <99550701+BinaryBae@users.noreply.github.com>
2023-03-14 00:25:20 +0530

    Updating sleeping_barbers.cpp

--- src/sleeping_barbers.cpp
+++ src/sleeping_barbers.cpp
@@ -1,57 +1,72 @@
 #include <bits/stdc++.h> 
-#include "semaphore.cpp"
+#include "Brutel5.cpp"
 #include <pthread.h>
 #include <unistd.h>
 
 using namespace std;
 
-#define MAX_CUSTOMERS 40
-#define NO_OF_BARBERS 5
-#define WAITING_CHAIRS 10
+#define MAX_CUSTOMERS 10
+#define NO_OF_BARBERS 3
+#define WAITING_CHAIRS 5
+#define MAX_ITERATIONS 10
 
 int waiting_customers = 0;
 Semaphore block = Semaphore(1);
 Semaphore mutex1 = Semaphore(1);
-
+Semaphore S = Semaphore(1);
 queue<int>customers;
 void* customer(void* args){
 	int identity = *(int*)args;
 	mutex1.wait();
 	sleep(1);
 	if (waiting_customers < WAITING_CHAIRS){
+		S.wait();
 		waiting_customers++;
 		customers.push(identity+1);		
+		printf("customer %d entered waiting area\n", identity+1);
+		S.release();
 		mutex1.release();
-		printf("customer %d entered waiting area\n", identity+1);		
+				
 	} 
 	else{				
-		mutex1.release();
 		printf("customer %d left without hair cut\n", identity+1);
+		mutex1.release();
+		
 	}
 	return NULL;
 }
 bool barbers_state[NO_OF_BARBERS];
 void* barber(void* args){
+
+	for (int i=0;i<MAX_ITERATIONS;i++){
 	int identity = *(int*)args;
 	block.wait();
 	sleep(1);
-	if (waiting_customers == 0){		
-		barbers_state[identity+1] = 1;
+	if (waiting_customers == 0){				
 		block.release();
-		printf("Barber %d going to sleep\n", identity+1);
+		if (!barbers_state[identity+1]){
+			barbers_state[identity+1] = 1;
+			printf("Barber %d going to sleep\n", identity+1);	
+		}
+		
 	}
 	else{		
+		S.wait();
 		waiting_customers--;
 		int customer_id = customers.front();
 		customers.pop();
-		block.release();
+		S.release();
 		if (barbers_state[identity+1])
 			printf("Barber %d wake up from sleep\n", identity+1);
 		barbers_state[identity+1] = 0;		
-		sleep(3);
+		
 		cout<<"Barber "<<identity+1<<" is going to cut the hairs of customer "<<customer_id<<'\n';
-		cout<<"customer "<<customer_id<<" is leaving after his hair cut\n";		
-	}
+		sleep(5);
+		cout<<"customer "<<customer_id<<" is leaving after his hair cut\n";
+		
+		block.release();
+				
+	}}
 	return NULL;
 }
 
@@ -78,5 +93,6 @@ int main(){
     for(int i = 0; i < MAX_CUSTOMERS; i++)
     {
         pthread_join(customers[i],NULL);
+        sleep(rand()%2);
     }
 }
